db8f2a2c2dd180a5ceac3deb2c095406e6db9cc8
Luiz Pereira <luckpecontato@gmail.com>
2023-05-01 18:00:18 -0300

    primeiros endpoints

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,43 @@
 import express from "express";
+import teams from "./teams";
+
+const OK = 200;
 
 const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => res.status(OK).json({ message: "Hello world!" }));
+
+app.get('/teams', (req, res) => res.status(200).json({ teams }));
+
+app.post('/teams', (req, res) => {
+  const newTeam = { ...req.body };
+  teams.push(newTeam);
+
+  res.status(201).json({ team: newTeam });
+});
+
+app.put('/teams/:id', (req, res) => {
+  const { id } = req.params;
+  const { name, initials } = req.body;
+
+  const updateTeam = teams.find((team) => team.id === Number(id));
+
+  if (!updateTeam) {
+    res.status(404).json({ message: 'Team not found' });
+  } else {
+    updateTeam.name = name;
+    updateTeam.initials = initials;
+    res.status(200).json({ updateTeam });
+  }
+});
+
+app.delete('/teams/:id', (req, res) => {
+  const { id } = req.params;
+  const arrayPosition = teams.findIndex((team) => team.id === Number(id));
+  teams.splice(arrayPosition, 1);
+
+  res.status(200).end();
+});
 
 export default app;
